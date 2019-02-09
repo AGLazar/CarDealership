8a2a4e40e500681f4cd12e627d76db91b5999077
=AGLazar <=alexandrugeorge.lazar@gmail.com>
2019-02-09 05:22:45 -0800

    AddedCarClass

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,57 @@
+package model;
+
+public class Car {
+    private String carId;
+    private String brand;
+    private String model;
+    private Integer price;
+    private String equipmentLevel;
+
+    public Car(String carId, String brand, String model, Integer price, String equipmentLevel) {
+        this.carId = carId;
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+        this.equipmentLevel = equipmentLevel;
+    }
+
+    public String getCarId() {
+        return carId;
+    }
+
+    public void setCarId(String carId) {
+        this.carId = carId;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getEquipmentLevel() {
+        return equipmentLevel;
+    }
+
+    public void setEquipmentLevel(String equipmentLevel) {
+        this.equipmentLevel = equipmentLevel;
+    }
+}
